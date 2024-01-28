cbf94bda0fe89d7ab7b2d594083c2a471ba0123b
jsdriscoll8 <jsdriscoll8@gmail.com>
2024-01-28 17:38:49 -0500

    added passwords CSV and password interface, fixed constructors

--- employee.py
+++ employee.py
@@ -1,43 +1,36 @@
 # The three subclasses of Employee represent the three specified required roles:
 # Admin, Engineer, and Intern.
 class Employee:
-    # Declare a new, default Employee.
-    def __init__(self):
-        self.username = "DEFAULT_USER"
-        self.password = "123456"
+    # Declare a new Employee.
+    def __init__(self, username=None, password=None):
+        if username is not None:
+            self.username = username
+        else:
+            self.username = "DEFAULT_USER"
 
-    # Declare a new Employee with a user-defined username and password.
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+        if password is not None:
+            self.password = password
+        else:
+            self.password = "PASSWORD"
+
 
 # Admin class: full management privileges.
 class Admin(Employee):
     # Create a new, default Admin.
-    def __init__(self):
-        super().__init__()
-
-    # Create a user-defined Admin.
-    def __int__(self, username, password):
+    def __init__(self, username=None, password=None):
         super().__init__(username, password)
+
 
 # Engineer class: access to project documents & personal timesheet, but not internal
 # accounting or management operations.
 class Engineer(Employee):
-    # Create a new, default Engineer.
-    def __init__(self):
-        super().__init__()
-
-    # Create a user-defined Engineer.
-    def __int__(self, username, password):
+    # Create a new Engineer.
+    def __init__(self, username=None, password=None):
         super().__init__(username, password)
+
 
 # Intern class: access only to personal timesheet, not live project documents or accounting.
 class Intern(Employee):
-    # Create a new, default Intern.
-    def __init__(self):
-        super().__init__()
-
-    # Create a user-defined Intern.
-    def __int__(self, username, password):
+    # Create a new Intern
+    def __int__(self, username=None, password=None):
         super().__init__(username, password)
--- logon.py
+++ logon.py
@@ -1,1 +1,54 @@
+import linecache
+
 import employee
+import io
+
+# Main logon program.
+def main():
+    # Open the stored users file and relay a greeting, asking the user for their username & password.
+    users_file = open("users.csv", "r")
+    print("Welcome to the company portal!")
+    username = input("Enter your username to continue, or type 'quit' to quit: ")
+    user_data_line = 0
+
+    # Quit on user command.
+    # Otherwise, look through the password file and find their logon information.
+    if username != 'quit':
+        user_data = None
+        username_match = False
+
+        # Search through the CSV file. Break from the search if the username is found.
+        while not username_match and username != 'quit':
+            user_data_line = 0
+            for user_line in users_file:
+                user_data_line += 1
+                user_data = user_line.split(",")[1]
+                if user_data == username:
+                    username_match = True
+                    break
+
+            # Ask again for
+            if not username_match:
+                username = input("Username not found. Try again, or type 'quit' to quit: ")
+
+    # If a valid username is entered, continue.
+    if username != 'quit':
+        # Get the user's role, and ask them for their password.
+        user_role = linecache.getline("users.csv", user_data_line).split(",")[0]
+        password = input("Please enter your password, or type 'quit' to quit: ")
+        while password != 'quit' and password != linecache.getline("users.csv", user_data_line).split(",")[2].rstrip():
+            password = input("Invalid password! Try again, or type 'quit' to quit: ")
+
+        if password != 'quit':
+            match user_role:
+                case 'Admin':
+                    admin = employee.Admin(username, password)
+
+
+    # Close the file, relay a farewell.
+    users_file.close()
+    print("Logging off...")
+
+
+if __name__ == '__main__':
+    main()
